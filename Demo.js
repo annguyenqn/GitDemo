56416f791e7fed824dd935b138de0a2c915e0666
An_Dev <theesan2k1@gmail.com>
2024-03-25 09:46:38 +0700

    feat:showage

--- Demo.js
+++ Demo.js
@@ -4,11 +4,16 @@ function hello() {
 function showName() {
     console.log('name2');
 }
+
 function showAddress() {
     console.log('qn');
 }
 function showAge() {
     console.log('20');
+}
+function showAge() {
+    console.log('25');
+
 }
 
 
