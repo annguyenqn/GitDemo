833f9f24ce503280deeca07d244a9b86b3d6dd4f
An_Dev <theesan2k1@gmail.com>
2024-03-27 17:22:57 +0700

    demo1

--- Demo.js
+++ Demo.js
@@ -1,3 +1,15 @@
 function hello() {
     console.log('Xin chao');
 }
+function hello2
+    () {
+    console.log('Xin chao');
+}
+function hello3
+    () {
+    console.log('Xin chao');
+}
+function hello4
+    () {
+    console.log('Xin chao');
+}
